44a346ab58275c0423d761bd9cb3a1d7c0c32e43
allyaale <aalea.ally@mail.utoronto.ca>
2020-11-25 11:31:10 -0500

    Implemented actions when key presses happen but they do not work

--- doodlejump.s
+++ doodlejump.s
@@ -29,6 +29,8 @@
 
 .data
 	displayAddress:.word 0x10008000
+	keyPressedListener: .word 0xffff0000 # 1 if a key has been pressed
+	keyPressed: .word 0xffff0004 # the ASCII value of the key that was pressed
 	
 	# offset of the leftmost pixel's location from the base address
 	platformOneLocation: .word 4020 
@@ -154,19 +156,49 @@ recolourPixelsOverDoodler:
 	lw $t0, displayAddress
 	lw $t1, backgroundColour
 	lw $t2, doodlerLocation
-	add $t3, $t0, $t2 # create $t3 starting at left-topmost 
-	subi $t3, $t3, 512 # pixel of doodler, will be used as cursor
+	add $t3, $t0, $t2 # create $t3 starting at left-bottommost pixel of doodler, will be used as cursor
 	
-	add $t4, $zero, $zero # set init value to 0
-	addi $t5, $zero, 5 # set loop stop val to 5 (loop repeats 5 times)
+	add $t4, $zero, $zero # set outer init value to 0
+	addi $t5, $zero, 5 # set outer loop stop val to 5 (outer loop repeats 5 times)
+	add $t6, $zero, $zero # set inner init value to 0
+	addi $t7, $zero, 5 # set inner loop stop val to 5 (inner loop repeats 5 times)
+START_OUTER_LOOP_DRAWING_OVER_DOODLER:	beq $t4, $t5, EXIT_OUTER_LOOP_DRAWING_OVER_DOODLER # branch if counter is 5
 
-START_LOOP_COLOUR_PIXELS_OVER_DOODLER:		beq $t4, $t5, EXIT_LOOP_COLOUR_PIXELS_OVER_DOODLER # branch if counter is 5
-						sw $t1, 0($t3) # paint the pixel at the cursor's address
-						addi $t3, $t3, 4 # increment the cursor by 4 to target the next address in display
-UPDATE_LOOP_COLOUR_PIXELS_OVER_DOODLER: 	addi $t4, $t4, 1 # increment counter by 1
-			 			j START_LOOP_COLOUR_PIXELS_OVER_DOODLER
-EXIT_LOOP_COLOUR_PIXELS_OVER_DOODLER: 		jr $ra
+START_INNER_LOOP_DRAWING_OVER_DOODLER:	beq $t6, $t7, EXIT_INNER_LOOP_DRAWING_OVER_DOODLER # branch if counter is 5
+					sw $t1, 0($t3) # paint the pixel at the cursor's address
+					addi $t3, $t3, 4 # increment the cursor by 4 to target the next address in display
+UPDATE_INNER_LOOP_DRAWING_OVER_DOODLER: 	addi $t6, $t6, 1 # increment counter by 1
+			 		j START_INNER_LOOP_DRAWING_OVER_DOODLER
+EXIT_INNER_LOOP_DRAWING_OVER_DOODLER: 	subi $t3, $t3, 20 # set cursor to first
+					subi $t3, $t3, 128 # pixel of next row
+					add $t6, $zero, $zero # reset inner init value to 0
+					
+UPDATE_OUTER_LOOP_DRAWING_OVER_DOODLER: 	addi $t4, $t4, 1 # increment counter by 1
+			 		j START_OUTER_LOOP_DRAWING_OVER_DOODLER
+EXIT_OUTER_LOOP_DRAWING_OVER_DOODLER: 
+	jr $ra
 
+keyPressHandler:
+	lw $t0, doodlerLocation
+	lw $t1, keyPressedListener
+	lw $t2, keyPressed # store the ASCII value of the key that was pressed
+			
+	# check for a key press
+	bne $t1, 1, KEY_NOT_PRESSED
+			
+	# check if the key press was 'j'
+	bne $t2, 106, NOT_J_KEY_PRESS
+	# handle j key press
+	addi $t0, $t0, -4 # move to left by 1 pixel
+	j KEY_NOT_PRESSED	
+			
+NOT_J_KEY_PRESS:	#check if the key press was 'k'
+			bne $t2, 107, KEY_NOT_PRESSED
+			#handle k key press
+			addi $t0, $t0, 4 # move to right by 1 pixel
+			
+KEY_NOT_PRESSED:	sw $t0, doodlerLocation
+			jr $ra
 
 jump:
 	addi $sp, $sp, -4
@@ -178,10 +210,10 @@ jump:
 	addi $t9, $zero, 7 # set loop stop val to 7 (loop repeats 7 times)
 START_LOOP_JUMP_UP:	beq $t8, $t9, EXIT_LOOP_JUMP_UP # branch if counter is 7
 			# 1. Update Doodler's position by 1 up
-			jal recolourPixelsUnderDoodler
+			jal recolourPixelsOverDoodler
+			jal keyPressHandler
 			
 			lw $t0, doodlerLocation
-			
 			subi $t0, $t0, 128
 			sw $t0, doodlerLocation
 	
@@ -199,9 +231,9 @@ EXIT_LOOP_JUMP_UP:
 START_LOOP_JUMP_DOWN:	beq $t8, $t9, EXIT_LOOP_JUMP_DOWN # branch if counter is 7
 			# 1. Update Doodler's position by 1 up
 			jal recolourPixelsOverDoodler
+			jal keyPressHandler
 			
 			lw $t0, doodlerLocation
-			
 			addi $t0, $t0, 128
 			sw $t0, doodlerLocation
 	
